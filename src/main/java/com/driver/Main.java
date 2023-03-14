1ca2cbcf1f6e7d2ed2c1ec32425b2b8b9e2476ae
yanipanay <33549205+yanipanay@users.noreply.github.com>
2023-03-14 10:28:01 +0530

    assignement attempt1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        // java: name has private access in com.driver.RWOnly
+
+        rw.setName("Yani");
+
+        String name1 = rw.getName();
+
+        System.out.println(name1);
+    }
 }
